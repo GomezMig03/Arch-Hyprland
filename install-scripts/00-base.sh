791893ca7cf95663561dd47c4281d6f999bb8b3b
Don Williams <don.e.williams@gmail.com>
2026-02-13 13:07:16 -0500

    Updated JAK references to LinuxBeginnings
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   .github/FUNDING.yml
    	modified:   .github/ISSUE_TEMPLATE/bug.yml
    	modified:   .github/ISSUE_TEMPLATE/documentation-update.yml
    	modified:   .github/ISSUE_TEMPLATE/feature.yml
    	modified:   .github/PULL_REQUEST_TEMPLATE.md
    	modified:   CHANGELOGS.md
    	modified:   CONTRIBUTING.md
    	modified:   README.md
    	modified:   assets/hyprland-install/actions.sh
    	modified:   assets/hyprland-install/scripts/install-hyprland-git.sh
    	modified:   assets/hyprland-install/scripts/install-hyprland.sh
    	modified:   assets/hyprland-install/scripts/uninstall.sh
    	modified:   auto-install.sh
    	modified:   install-scripts/00-base.sh
    	modified:   install-scripts/01-hypr-pkgs.sh
    	modified:   install-scripts/02-Final-Check.sh
    	modified:   install-scripts/Global_functions.sh
    	modified:   install-scripts/InputGroup.sh
    	modified:   install-scripts/ags.sh
    	modified:   install-scripts/battery-monitor.sh
    	modified:   install-scripts/bluetooth.sh
    	modified:   install-scripts/disk-monitor.sh
    	modified:   install-scripts/dotfiles-main.sh
    	modified:   install-scripts/fonts.sh
    	modified:   install-scripts/gtk_themes.sh
    	modified:   install-scripts/hyprland.sh
    	modified:   install-scripts/nvidia.sh
    	modified:   install-scripts/nvidia_nouveau.sh
    	modified:   install-scripts/pacman.sh
    	modified:   install-scripts/paru.sh
    	modified:   install-scripts/pipewire.sh
    	modified:   install-scripts/quickshell.sh
    	modified:   install-scripts/rog.sh
    	modified:   install-scripts/sddm.sh
    	modified:   install-scripts/sddm_theme.sh
    	modified:   install-scripts/temp-monitor.sh
    	modified:   install-scripts/thunar.sh
    	modified:   install-scripts/thunar_default.sh
    	modified:   install-scripts/xdph.sh
    	modified:   install-scripts/yay.sh
    	modified:   install-scripts/zsh.sh
    	modified:   install-scripts/zsh_pokemon.sh
    	modified:   install.sh
    	modified:   uninstall.sh

--- install-scripts/00-base.sh
+++ install-scripts/00-base.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 💫 https://github.com/JaKooLit 💫 #
+# 💫 https://github.com/LinuxBeginnings 💫 #
 # base-devel + archlinux-keyring #
 
 base=( 
--- install-scripts/pipewire.sh
+++ install-scripts/pipewire.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 💫 https://github.com/JaKooLit 💫 #
+# 💫 https://github.com/LinuxBeginnings 💫 #
 # Pipewire and Pipewire Audio Stuff #
 
 pipewire=(
